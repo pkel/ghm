af3a96a0127d5aff52e21643414ade0401700d9c
Patrik Keller <patrik@keller-re.de>
2017-02-05 12:50:29 +0100

    elm. Rafactor Database.elm

--- elm/src/Database.elm
+++ elm/src/Database.elm
@@ -1,11 +1,11 @@
 module Database exposing
-  ( getCustomerById
-  , getPrevCustomerById
-  , getNextCustomerById
-  , getLatestCustomer
-  , saveCustomer
-  , newCustomer
-  )
+    ( getCustomerById
+    , getPrevCustomerById
+    , getNextCustomerById
+    , getLatestCustomer
+    , saveCustomer
+    , newCustomer
+    )
 
 import Http
 import Json.Encode as Encode
@@ -15,10 +15,12 @@ import Config
 import Customer as C exposing (Customer)
 
 customersUri : String
-customersUri = Config.apiUrl ++ "/customers"
+customersUri =
+    Config.apiUrl ++ "/customers"
 
 customerSelect : String
-customerSelect = "&select=*,bookings{*,booked_individuals{*},booked_rooms{*}}"
+customerSelect =
+    "select=*,bookings{*,booked_individuals{*},booked_rooms{*}}"
 
 
 ---------------
@@ -27,80 +29,92 @@ customerSelect = "&select=*,bookings{*,booked_individuals{*},booked_rooms{*}}"
 
 getCustomerById : (Result Http.Error Customer -> msg) -> Int -> Cmd msg
 getCustomerById encap id =
-  let uri
-      = customersUri
-      ++ "?customer_id=eq."
-      ++ (toString id)
-      ++ customerSelect
-  in
-  Http.send encap (Http.get uri C.jsonDecoderFirst)
+    let params =
+            [ "customer_id=eq." ++ (toString id)
+            , customerSelect
+            ]
+
+        uri = buildUri customersUri params Nothing
+    in
+        Http.send encap (Http.get uri C.jsonDecoderFirst)
 
 getPrevCustomerById : (Result Http.Error Customer -> msg) -> String -> Int -> Cmd msg
 getPrevCustomerById encap filter id =
-  let uri
-      = customersUri
-      ++ "?customer_id=lt."
-      ++ (toString id)
-      ++ customerSelect
-      ++ "&order=customer_id.desc"
-      ++ "&limit=1"
-  in
-  let uri_ = appendKeywordFilter uri filter in
-  Http.send encap (Http.get uri_ C.jsonDecoderFirst)
+    let params =
+            [ "customer_id=lt." ++ (toString id)
+            , customerSelect
+            , "order=customer_id.desc"
+            , "limit=1"
+            ]
+
+        uri = buildUri customersUri params (Just filter)
+    in
+        Http.send encap (Http.get uri C.jsonDecoderFirst)
 
 getNextCustomerById : (Result Http.Error Customer -> msg) -> String -> Int -> Cmd msg
 getNextCustomerById encap filter id =
-  let uri
-      = customersUri
-      ++ "?customer_id=gt."
-      ++ (toString id)
-      ++ customerSelect
-      ++ "&order=customer_id.asc"
-      ++ "&limit=1"
-  in
-  let uri_ = appendKeywordFilter uri filter in
-  Http.send encap (Http.get uri_ C.jsonDecoderFirst)
+    let params =
+            [ "customer_id=gt." ++ (toString id)
+            , customerSelect
+            , "order=customer_id.asc"
+            , "limit=1"
+            ]
+
+        uri = buildUri customersUri params (Just filter)
+    in
+        Http.send encap (Http.get uri C.jsonDecoderFirst)
 
 getLatestCustomer : (Result Http.Error Customer -> msg) -> String -> Cmd msg
 getLatestCustomer encap filter =
-  let uri
-      = customersUri
-      ++ "?order=customer_id.desc"
-      ++ customerSelect
-      ++ "&limit=1"
-  in
-  let uri_ = appendKeywordFilter uri filter in
-  Http.send encap (Http.get uri_ C.jsonDecoderFirst)
+    let params =
+            [ "order=customer_id.desc"
+            , customerSelect
+            , "limit=1"
+            ]
+
+        uri = buildUri customersUri params (Just filter)
+    in
+        Http.send encap (Http.get uri C.jsonDecoderFirst)
 
 saveCustomer : (Result Http.Error Customer -> msg) -> Customer -> Int -> Cmd msg
 saveCustomer encap c id =
-  let uri
-      = customersUri
-      ++ "?customer_id=eq."
-      ++ (toString id)
-  in
-  let cc = { c | customer_id = Just id } in
-  let json = C.jsonEncode cc in
-  Http.send encap (patchJson uri json C.jsonDecoderFirst)
+    let params =
+            [ "customer_id=eq." ++ (toString id)
+            ]
+
+        uri = buildUri customersUri params Nothing
+
+        cc = { c | customer_id = Just id }
+
+        json = C.jsonEncode cc
+    in
+        Http.send encap (patchJson uri json C.jsonDecoderFirst)
 
 newCustomer : (Result Http.Error Customer -> msg) -> Customer -> Cmd msg
 newCustomer encap c =
-  let cc = { c | customer_id = Nothing } in
-  let json = C.jsonEncode cc in
-  Http.send encap (postJson customersUri json C.jsonDecoder)
+    let cc = { c | customer_id = Nothing }
+
+        json = C.jsonEncode cc
+    in
+        Http.send encap (postJson customersUri json C.jsonDecoder)
 
 
 ----------------------
 -- Common Functions --
 ----------------------
 
-appendKeywordFilter : String -> String -> String
-appendKeywordFilter uri filter =
-  case String.trim filter of
-    "" -> uri
-    str ->
-        let str_ = Http.encodeUri ("%" ++ str ++ "%")
-        in uri ++ "&keyword=ilike." ++ str_
+
+buildUri : String -> List String -> Maybe String -> String
+buildUri base params filter =
+    let params_ =
+            case String.trim (Maybe.withDefault "" filter) of
+                "" -> params
+                str -> let kw = Http.encodeUri "%" ++ str ++ "%" in
+                    ("keyword=ilike." ++ kw) :: params
+    in
+        case String.join "&" params_ of
+            "" -> base
+            str -> String.concat [base, "?", str]
 
 
 -- Helpers
@@ -108,11 +122,11 @@ appendKeywordFilter uri filter =
 
 emptyJson : Encode.Value
 emptyJson =
-  Encode.object []
+    Encode.object []
 
 emptyJsonList : Encode.Value
 emptyJsonList =
-  Encode.list []
+    Encode.list []
 
 
 -- Http Requests
@@ -120,31 +134,30 @@ emptyJsonList =
 
 patchJson : String -> Encode.Value -> Decode.Decoder a -> Http.Request a
 patchJson uri json decoder =
-  let postgrestFullResponseHeader =
-    Http.header "Prefer" "return=representation"
-  in
-  Http.request
-    { method = "PATCH"
-    , headers = [postgrestFullResponseHeader]
-    , url = uri
-    , body = Http.jsonBody json
-    , expect = Http.expectJson decoder
-    , timeout = Nothing
-    , withCredentials = False
-    }
+    let postgrestFullResponseHeader =
+            Http.header "Prefer" "return=representation"
+    in
+        Http.request
+            { method = "PATCH"
+            , headers = [postgrestFullResponseHeader]
+            , url = uri
+            , body = Http.jsonBody json
+            , expect = Http.expectJson decoder
+            , timeout = Nothing
+            , withCredentials = False
+            }
 
 postJson : String -> Encode.Value -> Decode.Decoder a -> Http.Request a
 postJson uri json decoder =
-  let postgrestFullResponseHeader =
-    Http.header "Prefer" "return=representation"
-  in
-  Http.request
-    { method = "POST"
-    , headers = [postgrestFullResponseHeader]
-    , url = uri
-    , body = Http.jsonBody json
-    , expect = Http.expectJson decoder
-    , timeout = Nothing
-    , withCredentials = False
-    }
-
+    let postgrestFullResponseHeader =
+            Http.header "Prefer" "return=representation"
+    in
+        Http.request
+            { method = "POST"
+            , headers = [postgrestFullResponseHeader]
+            , url = uri
+            , body = Http.jsonBody json
+            , expect = Http.expectJson decoder
+            , timeout = Nothing
+            , withCredentials = False
+            }
