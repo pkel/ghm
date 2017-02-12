250bfac3152d7b1c42c4a9088cf0b490ed9205c5
Patrik Keller <patrik@keller-re.de>
2017-02-12 16:36:01 +0100

    elm. Beautify Customer.elm

--- elm/src/Customer.elm
+++ elm/src/Customer.elm
@@ -1,4 +1,10 @@
-module Customer exposing (Customer, jsonDecoder, jsonDecoderFirst, jsonEncode, empty)
+module Customer exposing
+    ( Customer
+    , jsonDecoder
+    , jsonDecoderFirst
+    , jsonEncode
+    , empty
+    )
 
 import Json.Decode as Decode exposing (Decoder)
 import Json.Decode.Pipeline as Pipeline
@@ -7,136 +13,138 @@ import Json.Encode.Extra as EncodeX
 
 import Booking as B exposing (Booking)
 
-type alias Customer = {
-  customer_id      : Maybe Int,
+type alias Customer =
+    { customer_id      : Maybe Int
 
-  title            : String,
-  title_letter     : String,
+    , title            : String
+    , title_letter     : String
 
-  given            : String,
-  second           : String,
-  family           : String,
+    , given            : String
+    , second           : String
+    , family           : String
 
-  company          : String,
-  company_address  : String,
+    , company          : String
+    , company_address  : String
 
-  street           : String,
-  street_number    : String,
-  postal_code      : String,
-  city             : String,
-  country          : String,
-  country_code     : String,
+    , street           : String
+    , street_number    : String
+    , postal_code      : String
+    , city             : String
+    , country          : String
+    , country_code     : String
 
-  phone            : String,
-  phone2           : String,
-  mobile           : String,
-  fax              : String,
-  fax2             : String,
-  mail             : String,
-  mail2            : String,
-  web              : String,
+    , phone            : String
+    , phone2           : String
+    , mobile           : String
+    , fax              : String
+    , fax2             : String
+    , mail             : String
+    , mail2            : String
+    , web              : String
 
-  keyword          : String,
-  note             : String,
+    , keyword          : String
+    , note             : String
 
-  bookings         : List Booking
-}
+    , bookings         : List Booking
+    }
 
 
 -- Json
 
 jsonDecoderFirst : Decoder Customer
 jsonDecoderFirst =
-  Decode.index 0 jsonDecoder
+    Decode.index 0 jsonDecoder
 
 jsonDecoder : Decoder Customer
 jsonDecoder =
-  let required = Pipeline.required in
-  let optional = Pipeline.optional in
-  let nullable = Decode.nullable in
-  let int      = Decode.int in
-  let string   = Decode.string in
-  let list     = Decode.list in
-  let booking  = B.jsonDecoder in
-  Pipeline.decode Customer
-    |> required "customer_id"      (nullable int)
+    let required = Pipeline.required
+        optional = Pipeline.optional
+        nullable = Decode.nullable
+        int      = Decode.int
+        string   = Decode.string
+        list     = Decode.list
+        booking  = B.jsonDecoder
+    in
+        Pipeline.decode Customer
+            |> required "customer_id"      (nullable int)
 
-    |> optional "title"            string    ""
-    |> optional "title_letter"     string    ""
+            |> optional "title"            string    ""
+            |> optional "title_letter"     string    ""
 
-    |> optional "given"            string    ""
-    |> optional "second"           string    ""
-    |> optional "family"           string    ""
+            |> optional "given"            string    ""
+            |> optional "second"           string    ""
+            |> optional "family"           string    ""
 
-    |> optional "company"          string    ""
-    |> optional "company_address"  string    ""
+            |> optional "company"          string    ""
+            |> optional "company_address"  string    ""
 
-    |> optional "street"           string    ""
-    |> optional "street_number"    string    ""
-    |> optional "postal_code"      string    ""
-    |> optional "city"             string    ""
-    |> optional "country"          string    ""
-    |> optional "country_code"     string    ""
+            |> optional "street"           string    ""
+            |> optional "street_number"    string    ""
+            |> optional "postal_code"      string    ""
+            |> optional "city"             string    ""
+            |> optional "country"          string    ""
+            |> optional "country_code"     string    ""
 
-    |> optional "phone"            string    ""
-    |> optional "phone2"           string    ""
-    |> optional "mobile"           string    ""
-    |> optional "fax"              string    ""
-    |> optional "fax2"             string    ""
-    |> optional "mail"             string    ""
-    |> optional "mail2"            string    ""
-    |> optional "web"              string    ""
+            |> optional "phone"            string    ""
+            |> optional "phone2"           string    ""
+            |> optional "mobile"           string    ""
+            |> optional "fax"              string    ""
+            |> optional "fax2"             string    ""
+            |> optional "mail"             string    ""
+            |> optional "mail2"            string    ""
+            |> optional "web"              string    ""
 
-    |> optional "keyword"          string    ""
-    |> optional "note"             string    ""
+            |> optional "keyword"          string    ""
+            |> optional "note"             string    ""
 
-    |> optional "bookings"         (list booking) []
+            |> optional "bookings"         (list booking) []
 
 
 jsonEncode : Customer -> Encode.Value
 jsonEncode c =
-  let int    = Encode.int in
-  let maybe  = EncodeX.maybe in
-  let string = Encode.string in
-  Encode.object
-    [ ("customer_id",      (maybe int) c.customer_id)
+    let int    = Encode.int
+        maybe  = EncodeX.maybe
+        string = Encode.string
+    in
+        Encode.object
+            [ ("customer_id",      (maybe int) c.customer_id)
 
-    , ("title",            string c.title)
-    , ("title_letter",     string c.title_letter)
+            , ("title",            string c.title)
+            , ("title_letter",     string c.title_letter)
 
-    , ("given",            string c.given)
-    , ("second",           string c.second)
-    , ("family",           string c.family)
+            , ("given",            string c.given)
+            , ("second",           string c.second)
+            , ("family",           string c.family)
 
-    , ("company",          string c.company)
-    , ("company_address",  string c.company_address)
+            , ("company",          string c.company)
+            , ("company_address",  string c.company_address)
 
-    , ("street",           string c.street)
-    , ("street_number",    string c.street_number)
-    , ("postal_code",      string c.postal_code)
-    , ("city",             string c.city)
-    , ("country",          string c.country)
-    , ("country_code",     string c.country_code)
+            , ("street",           string c.street)
+            , ("street_number",    string c.street_number)
+            , ("postal_code",      string c.postal_code)
+            , ("city",             string c.city)
+            , ("country",          string c.country)
+            , ("country_code",     string c.country_code)
 
-    , ("phone",            string c.phone)
-    , ("phone2",           string c.phone2)
-    , ("mobile",           string c.mobile)
-    , ("fax",              string c.fax)
-    , ("fax2",             string c.fax2)
-    , ("mail",             string c.mail)
-    , ("mail2",            string c.mail2)
-    , ("web",              string c.web)
+            , ("phone",            string c.phone)
+            , ("phone2",           string c.phone2)
+            , ("mobile",           string c.mobile)
+            , ("fax",              string c.fax)
+            , ("fax2",             string c.fax2)
+            , ("mail",             string c.mail)
+            , ("mail2",            string c.mail2)
+            , ("web",              string c.web)
 
-    , ("keyword",          string c.keyword)
-    , ("note",             string c.note)
-    ]
+            , ("keyword",          string c.keyword)
+            , ("note",             string c.note)
+            ]
 
 
 -- Constructors
 
 empty : () -> Customer
 empty () =
-  Customer Nothing
+    Customer Nothing
     "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" ""
-    []
+        []
 
