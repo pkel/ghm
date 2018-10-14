a12b1b565e66fb8435a070b49f3d4e6167c37fb8
Patrik Keller <patrik@keller-re.de>
2018-10-14 21:11:19 +0200

    Improved Request API

--- gui/request.ml
+++ gui/request.ml
@@ -1,6 +1,4 @@
-type response = (Yojson.Safe.json, int) result
-
-type 'a handler = 'a -> unit
+open Core_kernel
 
 type verb = GET | POST | PUT | PATCH | DELETE
 
@@ -11,25 +9,63 @@ let verb = function
   | PATCH -> "PATCH"
   | DELETE -> "DELETE"
 
-let send ?body ?prefer ~v return url =
-  let open Browser.XHR in
+type ('a, 'b) t =
+  { body: 'a -> string
+  ; resp: string -> 'b Or_error.t
+  ; xhr: Browser.XHR.t
+  ; url: string
+  (* for error *) }
+
+open Browser.XHR
+
+let create ~v ~url =
   let xhr = create () in
   open_ xhr (verb v) url ;
-  set_request_header xhr "accept" "application/json" ;
-  (match prefer with None -> () | Some p -> set_request_header xhr "Prefer" p) ;
-  set_onreadystatechange xhr (fun () ->
-      match ready_state xhr with
+  {xhr; resp= (fun s -> Ok s); body= (fun () -> ""); url}
+
+let conv_resp ~f t = {t with resp= Fn.compose (Or_error.bind ~f) t.resp}
+
+let map_resp ~f t = {t with resp= Fn.compose (Or_error.map ~f) t.resp}
+
+let map_body ~f t = {t with body= Fn.compose t.body f}
+
+let want_json t =
+  set_request_header t.xhr "accept" "application/json" ;
+  conv_resp t ~f:(fun str ->
+      try Ok (Yojson.Safe.from_string str) with e -> Error (Error.of_exn e) )
+
+let give_json t =
+  set_request_header t.xhr "Content-Type" "application/json" ;
+  {t with body= Yojson.Safe.to_string}
+
+let give_text t =
+  set_request_header t.xhr "Content-Type" "application/json" ;
+  {t with body= Fn.id}
+
+let prefer str t =
+  set_request_header t.xhr "Prefer" str ;
+  t
+
+let send' ~handler t =
+  set_onreadystatechange t.xhr (fun () ->
+      match ready_state t.xhr with
       | Done ->
-          let status = status xhr in
+          let status = status t.xhr in
           if 200 <= status && status < 300 then
-            Ok (Yojson.Safe.from_string (response_text xhr)) |> return
-          else Error status |> return
+            t.resp (response_text t.xhr) |> handler
+          else
+            Or_error.errorf "request on %s failed with %i" t.url status
+            |> handler
       | _ -> () ) ;
-  let body =
-    match body with
-    | None -> ""
-    | Some j ->
-        set_request_header xhr "Content-Type" "application/json" ;
-        Yojson.Safe.to_string j
-  in
-  send xhr body
+  send t.xhr (t.body ())
+
+let send ~body ~handler t =
+  set_onreadystatechange t.xhr (fun () ->
+      match ready_state t.xhr with
+      | Done ->
+          let status = status t.xhr in
+          if 200 <= status && status < 300 then
+            t.resp (response_text t.xhr) |> handler
+          else Or_error.errorf "XHR failed with %i" status |> handler
+      | _ -> () ) ;
+  send t.xhr (t.body body)
--- gui/request.mli
+++ gui/request.mli
@@ -1,13 +1,28 @@
-type response = (Yojson.Safe.json, int) result
+open Yojson.Safe
+open Core_kernel
 
-type 'a handler = 'a -> unit
+(** A request with body ['a] and response type ['b]. *)
+type ('a, 'b) t
 
 type verb = GET | POST | PUT | PATCH | DELETE
 
-val send :
-     ?body:Yojson.Safe.json
-  -> ?prefer:string
-  -> v:verb
-  -> response handler
-  -> string
-  -> unit
+val create : v:verb -> url:string -> (unit, string) t
+
+val give_json : (unit, 'a) t -> (json, 'a) t
+
+val give_text : (unit, 'a) t -> (string, 'a) t
+
+val want_json : ('a, string) t -> ('a, json) t
+
+val prefer : string -> ('a, 'b) t -> ('a, 'b) t
+(** Set the prefer header. *)
+
+val conv_resp : f:('b -> 'c Or_error.t) -> ('a, 'b) t -> ('a, 'c) t
+
+val map_resp : f:('b -> 'c) -> ('a, 'b) t -> ('a, 'c) t
+
+val map_body : f:('c -> 'a) -> ('a, 'b) t -> ('c, 'b) t
+
+val send : body:'a -> handler:('b Or_error.t -> unit) -> ('a, 'b) t -> unit
+
+val send' : handler:('b Or_error.t -> unit) -> (unit, 'b) t -> unit
