a799dc7897b7a7c1d53f084594cfc7241ad80a2f
Patrik Keller <patrik@keller-re.de>
2019-01-08 21:16:06 +0100

    Improve letter preview / printing.

--- letter/js/script.js
+++ letter/js/script.js
@@ -1,45 +1,53 @@
-function get_el (id) {
-  return document.getElementById(id).innerHTML
+var fields = ['sender','address','subject','sidebar','body','attachments'];
+
+function fieldId (fld) {
+  return "editable-" + fld;
 }
 
-function set_el (id, html) {
-  document.getElementById(id).innerHTML = html
-}
-
-function get_state () {
+function getState () {
   var state = { };
-  state.sender = get_el("editable-sender");
-  state.address = get_el("editable-address");
-  state.subject = get_el("editable-subject");
-  state.sidebar = get_el("editable-sidebar");
-  state.body = get_el("editable-body");
-  state.attachments = get_el("editable-attachments");
+  fields.forEach(function (el) {
+    state[el] = document.getElementById(fieldId(el)).innerHTML;
+  });
   return state;
 }
 
-function set_state (state) {
-  set_el("editable-sender" , state.sender );
-  set_el("editable-address", state.address);
-  set_el("editable-subject", state.subject);
-  set_el("editable-sidebar", state.sidebar);
-  set_el("editable-body"   , state.body   );
+function setState (state) {
+  fields.forEach(function (el) {
+    document.getElementById(fieldId(el)).innerHTML = state[el];
+  });
 }
 
 // safe content to hash for bookmarking
+var lastSave
 function safe () {
-  document.location.hash = btoa(JSON.stringify(get_state()));
+  lastSave = btoa(JSON.stringify(getState()));
+  document.location.hash = lastSave;
 }
 
-// trigger safe after 1 seconds of no input
-var timer
-document.body.addEventListener("input", function() {
-  window.clearTimeout(timer);
-  timer = setTimeout(safe, 1000);
-}, false);
-
-// init content from hash
-var b64 = document.location.hash.substr(1);
-if (b64.length > 0) {
-  var state = JSON.parse(window.atob(b64));
-  set_state(state);
+// load content from hash
+function load () {
+  var b64 = document.location.hash.substr(1);
+  if (b64.length > 0 && lastSave != b64) {
+    var state = JSON.parse(window.atob(b64));
+    setState(state);
+  }
 }
+
+function main () {
+  // trigger safe after 1 seconds of no input
+  var timer
+  document.body.addEventListener("input", function() {
+    window.clearTimeout(timer);
+    timer = setTimeout(safe, 1000);
+
+  }, false);
+
+  // respect forward/backward navigation
+  window.onhashchange = load;
+
+  load ();
+  safe ();
+}
+
+main ();
