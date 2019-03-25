23e783d5362c6adab7934e2190d379f3021768f0
Patrik Keller <patrik@keller-re.de>
2019-03-25 17:44:53 +0100

    Show status on login page

--- webroot/index.php
+++ webroot/index.php
@@ -30,18 +30,40 @@ function print_app() { ?>
 </html>
 <?php }
 
-function print_login() { ?>
+function print_alert_box($class, $msg) { ?>
+<div class="alert alert-<?php print($class) ?>" role="alert">
+  <?php print($msg . "\n") ?>
+</div>
+<?php }
+
+function print_status($status) {
+  switch ($status) {
+  case "login-fail":
+    print_alert_box("danger", "Nutzername ungültig oder Passwort falsch");
+    break;
+  case "logout-success":
+    print_alert_box("info", "Erfolgreich abgemeldet");
+    break;
+  }
+}
+
+function print_login($status) { ?>
 <!DOCTYPE html>
 <html lang="de">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script>
+// Get parameters are only for communicating status message. Drop.
+window.history.replaceState({}, document.title, window.location.href.split('?')[0]);
+    </script>
   </head>
   <body>
     <div class="container-fluid">
       <div class="row justify-content-center align-items-center" style="height:100vh">
         <div class="col-4">
+<?php print_status($status) ?>
           <div class="card">
             <div class="card-body">
               <form action="." method="POST" autocomplete="off">
@@ -81,11 +103,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     fail(400);
   }
 } elseif ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $filter_def = array( 'status' => FILTER_SANITIZE_STRING);
+  $get = filter_var_array($_GET, $filter_def);
   // TODO: if logged in print_app else
   if (isset($_SESSION['role'])) {
     print_app ();
   } else {
-    print_login ();
+    print_login ($get['status']);
   }
 } else {
   fail(405);
