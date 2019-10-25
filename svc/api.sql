76602721a1796103d28a92352e90488b9cc4dcfa
Patrik Keller <patrik@keller-re.de>
2019-10-25 22:46:20 +0200

    Draft new database

--- svc/api.sql
+++ svc/api.sql
@@ -1,14 +1,26 @@
-/* customers */
-
 set search_path=api;
 
 create table customers (
-    customer_id bigint generated always as identity primary key,
+    id bigint generated always as identity primary key,
     created timestamp not null default now (),
     modified timestamp not null default now (),
-    data jsonb not null);
+    data jsonb not null,
+    keyword text not null);
 
--- automatic ids
+create table bookings (
+    id bigint generated always as identity primary key,
+    customer bigint references customers,
+    created timestamp not null default now (),
+    modified timestamp not null default now (),
+    data jsonb not null,
+    arrival date not null,
+    departure date not null);
+
+create table invoice_numbers (
+    number text primary key,
+    booking bigint references bookings);
+
+/* automatic modification timestamps */
 create or replace function set_modified_now ()
 returns trigger as $$
 begin
@@ -20,7 +32,5 @@ $$ language 'plpgsql';
 create trigger customers_set_modified_now before update on customers
 for each row execute procedure set_modified_now ();
 
--- keyword search
-create or replace function keyword(customers) returns text as $$
-  select $1.data->>'keyword';
-$$ language sql;
+create trigger bookings_set_modified_now before update on bookings
+for each row execute procedure set_modified_now ();
