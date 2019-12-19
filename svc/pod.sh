8366d1a971d1052ded99a9f224ff99a97a96d84e
Patrik Keller <patrik@keller-re.de>
2019-12-19 17:59:09 +0100

    Prepare docker support

--- svc/pod.sh
+++ svc/pod.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
+set -euxo pipefail
+
+docker=${DOCKER:-podman}
+prefix=${PREFIX:-ghm}
+
 secret () {
   cat "./secrets/$1"
 }
 
-set -xe
+image () {
+  echo "$prefix-image-$1"
+}
 
-podman build -f Dockerfile-for-php -t ghm_php
-podman build -f Dockerfile-for-nginx -t ghm_nginx
+container () {
+  echo "$prefix-container-$1"
+}
 
-if podman pod exists ghm
-then
-  podman pod rm ghm -f
-fi
-
-podman pod create --name ghm -p 8080:80
-
-rm_container () {
-  if podman container exists "$1"
+link () {
+  if [[ -v ROOT ]]
   then
-    podman rm "$1"
+    echo "$prefix-container-$1"
+  else
+    echo "localhost"
   fi
 }
 
-rm_container ghm_db
-podman run -d --pod ghm --name ghm_db \
+if [[ -v ROOT ]]
+then
+  pod_options=()
+else
+  if $docker pod exists "$prefix-pod"
+  then
+    $docker pod rm -f "$prefix-pod"
+  fi
+  $docker pod create --name "$prefix-pod" -p 8080:80
+  pod_options=(--pod "$prefix-pod")
+fi
+
+$docker build -f Dockerfile-for-php -t "$(image php)"
+$docker build -f Dockerfile-for-nginx -t "$(image nginx)"
+
+rm_container () {
+  if $docker container exists "$(container "$1")"
+  then
+    $docker rm "$(container "$1")" -f
+  fi
+}
+
+rm_container db
+$docker run -d --name "$(container db)" \
+  "${pod_options[@]}" \
   -e POSTGRES_DB_FILE=/secrets/db-cfg/db-name \
   -e POSTGRES_USER_FILE=/secrets/db-user-root/username \
   -e POSTGRES_PASSWORD_FILE=/secrets/db-user-root/password \
@@ -32,43 +58,41 @@ podman run -d --pod ghm --name ghm_db \
   -v ./initdb.d:/docker-entrypoint-initdb.d:z \
   postgres:12
 
-rm_container ghm_api
-podman run -d --pod ghm --name ghm_api \
+rm_container api
+$docker run -d --name "$(container api)" \
+  "${pod_options[@]}" \
   -e PGRST_SERVER_HOST='*4' \
   -e PGRST_SERVER_PORT="3000" \
   -e PGRST_DB_ANON_ROLE="anonymous" \
   -e PGRST_DB_SCHEMA="api" \
-  -e PGRST_DB_URI="postgres://$(secret db-user-api/username):$(secret db-user-api/password)@localhost:5432/$(secret db-cfg/db-name)" \
+  -e PGRST_DB_URI="postgres://$(secret db-user-api/username):$(secret db-user-api/password)@$(link db):5432/$(secret db-cfg/db-name)" \
   -e PGRST_JWT_SECRET="$(secret jwt/secret)" \
   -e PGRST_SECRET_IS_BASE64="false" \
   postgrest/postgrest
 
-rm_container ghm_php
-rm_container ghm_nginx
-
-if [ -z "$MOUNT_WEBROOT" ]
+if [[ -v MOUNT_WEBROOT ]]
 then
-  podman run -d --pod ghm --name ghm_php \
-    -e DB_HOST=localhost \
-    -e DB_PORT=5432 \
-    -e DB_NAME="$(secret db-cfg/db-name)" \
-    -e DB_USER="$(secret db-user-auth/username)" \
-    -e DB_PASS="$(secret db-user-auth/password)" \
-    -e JWT_SECRET="$(secret jwt/secret)" \
-    ghm_php
-  podman run -d --pod ghm --name ghm_nginx \
-    ghm_nginx
+  mount_options=(-v ./webroot:/var/www/html:z)
 else
-  podman run -d --pod ghm --name ghm_php \
-    -e DB_HOST=localhost \
-    -e DB_PORT=5432 \
-    -e DB_NAME="$(secret db-cfg/db-name)" \
-    -e DB_USER="$(secret db-user-auth/username)" \
-    -e DB_PASS="$(secret db-user-auth/password)" \
-    -e JWT_SECRET="$(secret jwt/secret)" \
-    -v ./webroot:/var/www/html:z \
-    ghm_php
-  podman run -d --pod ghm --name ghm_nginx \
-    -v ./webroot:/var/www/html:z \
-    ghm_nginx
+  mount_options=()
 fi
+
+rm_container php
+podman run -d --name "$(container php)" \
+  "${pod_options[@]}" \
+  -e DB_HOST="$(link db)" \
+  -e DB_PORT=5432 \
+  -e DB_NAME="$(secret db-cfg/db-name)" \
+  -e DB_USER="$(secret db-user-auth/username)" \
+  -e DB_PASS="$(secret db-user-auth/password)" \
+  -e JWT_SECRET="$(secret jwt/secret)" \
+  "${mount_options[@]}" \
+  "$(image php)"
+
+rm_container nginx
+podman run -d --name "$(container nginx)" \
+  "${pod_options[@]}" \
+  -e PHP_HOST="$(link php)" \
+  -e API_HOST="$(link api)" \
+  "${mount_options[@]}" \
+  "$(image nginx)"
